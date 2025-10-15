bc09e48a451fa08122fcbf7da44859c1c9995a5c
watanabethiti <thitiporn.srir@ku.th>
2025-10-16 00:36:31 +0700

    แก้ final

--- projectDesign/src/main/java/com/mycompany/projectdesign/Project/StrategyPattern/DiscountSelector.java
+++ projectDesign/src/main/java/com/mycompany/projectdesign/Project/StrategyPattern/DiscountSelector.java
@@ -18,11 +18,12 @@ public class DiscountSelector {
      */
     public static DiscountStrategy getStrategy(Room room, Bookings booking) {
         
-        if (booking.getDateStay() >= 2) {
-            return new StayMoreDiscount();
-        } else if (booking.getDaysBeforeCheckin() >= 30) {
+        if (booking.getDaysBeforeCheckin() >= 30) {
             return new EarlyBirdDiscount();
+        } else if (booking.getDateStay() >= 2) {
+            return new StayMoreDiscount();
         }
         return new DefaultDiscount();
     }
 }
+
